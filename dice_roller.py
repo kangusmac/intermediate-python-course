8d9f59cf2d3ba54a1cb5b6e700bc706aff01225d
kenneth macsween <kangusmac@gmail.com>
2020-04-05 12:13:07 +0200

    change values by input

--- dice_roller.py
+++ dice_roller.py
@@ -1,13 +1,14 @@
 def main():
   import random
-  dice_rolls = 2
+  dice_rolls = int(input('How many dices would you like ti roll?'))
+  dice_sizes = int(input('How many sides are the dice?'))
   dice_sum   = 0
   for i in range(0,dice_rolls):
     roll = random.randint(1,6)
     dice_sum += roll
     if roll == 1:
       print(f'You rolled a {roll} ! Critical Fail')
-    elif roll == 6:
+    elif roll == dice_sizes:
       print(f'You rolled a {roll} ! Critical success!')
     else:
       print(f'You rolled a {roll}')
